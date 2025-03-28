40f79796b58ce84c725db19bcbb2189052e6599d
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-03-28 10:09:51 +0100

    Update pesel.py

--- code/pesel.py
+++ code/pesel.py
@@ -15,11 +15,25 @@ arrMonth = {
     12: "Grudzień"
 }
 
+def calculateControlSum(user_input):
+    val = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+    controlSum = 0
+    for i in range(len(val)):
+        controlSum += val[i] * int(user_input[i])
+    controlSum = (10 - (controlSum % 10)) % 10  
+
+    if int(user_input[10]) != controlSum:
+        print(f"Invalid control num (expected {controlSum}, got {user_input[10]})")
+    else:
+        print("Valid control num")
+
 def validate(user_input):
-    pesel_regex = re.compile(r"^\d{2}[0-9][0-9][0-3][0-9]\d{4}\d$")
-    if len(user_input) != 11 or not pesel_regex.match(user_input):
+    pesel_regex = re.compile(r"^\d{11}$")  
+    if not pesel_regex.match(user_input):
         print("Invalid pesel format")
         return False
+    calculateControlSum(user_input)
+    
     return True
 
 def makeDate(user_input):
@@ -27,37 +41,39 @@ def makeDate(user_input):
     sex = ""
     month = ""
     year = ""
-    if user_input[2] == "0" or user_input[2] == "1":
-        year = "19" + user_input[0] + user_input[1]
-    elif user_input[2] == "2" or user_input[2] == "3":
-        year = "20" + user_input[0] + user_input[1]
-    elif user_input[4] == "2" or user_input[5] == "1":
-        year = "21" + user_input[0] + user_input[1]
-    elif user_input[6] == "2" or user_input[7] == "1":
-        year = "18" + user_input[0] + user_input[1]
-    
-    month = user_input[2:4]
-    finalMonth = int(month) - 20
 
-    if finalMonth not in arrMonth.keys():
-        print(f"Invalid month value: {month}. Change digits at positions 3 and 4.")
-        return "Invalid Pesel in position 3 and 4 "
 
-    print(f"Final month: {finalMonth}")
-    
+    month_code = int(user_input[2:4])
+    if 1 <= month_code <= 12:
+        year = "19" + user_input[0:2]
+    elif 21 <= month_code <= 32:
+        year = "20" + user_input[0:2]
+        month_code -= 20
+    elif 41 <= month_code <= 52:
+        year = "21" + user_input[0:2]
+        month_code -= 40
+    elif 61 <= month_code <= 72:
+        year = "18" + user_input[0:2]
+        month_code -= 60
+    else:
+        print(f"Invalid month value: {month_code}. Change digits at positions 3 and 4.")
+        return "Invalid Pesel in position 3 and 4"
+
+    print(f"Final month: {month_code}")
+
     if int(user_input[9]) % 2 == 0:
         sex = "Kobieta"
     else:
         sex = "Mężczyzna"
-    
+
     day = user_input[4:6]
-    date = sex + " " + "Rok: " + year + " Miesiac: " + str(finalMonth) + " Dzień: " + str(user_input[4:6]) + "\n"
-    diffMonthFormat = arrMonth[finalMonth]
-    diffDateFormat = "Nowy Format: " + sex + " " + str(user_input[4:6]) + " " + diffMonthFormat + " " + year
+    date = sex + " " + "Rok: " + year + " Miesiac: " + str(month_code) + " Dzień: " + str(day) + "\n"
+    diffMonthFormat = arrMonth[month_code]
+    diffDateFormat = "Nowy Format: " + sex + " " + str(day) + " " + diffMonthFormat + " " + year
 
     print(date)
     print(diffDateFormat)
-    selectFormat(day, finalMonth, year, sex)
+    selectFormat(day, month_code, year, sex)
 
 def selectFormat(day, month, year, sex):
     print("\nWybierz format daty:")
@@ -75,15 +91,12 @@ def selectFormat(day, month, year, sex):
     else:
         print("Niepoprawny wybór.")
 
-
 userEndFlag = True
 while userEndFlag:
     user_input = input("Enter pesel:").strip()
 
-
     if not validate(user_input):
         continue
-
 
     makeDate(user_input)
 
